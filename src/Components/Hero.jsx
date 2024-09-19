c03fe3aacdbec907c7e483c1403aa909f183493f
VikramGujar <vikramgujar300@gmail.com>
2024-09-19 23:56:12 +0530

    Header of portfolio

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section 
+    id='home'
+    className=''
+    >
+        <div className=''>
+
+        
+        <div className=''>
+    <figure className=''>
+        <img 
+        src="" 
+        alt="Vikram"
+        className='img-cover'
+        />
+    </figure>
+    </div>
+
+    <div/>
+    <div className=''>
+        <span className=''>
+            <span className=''></span>
+        </span>
+    </div>
+
+        </div>
+      
+    </section>
+  )
+}
+
+export default Hero
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
